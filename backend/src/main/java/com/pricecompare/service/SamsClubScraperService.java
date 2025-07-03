9c0713cc0a5c3aa76019527ff69138e7b0ed9e71
Armando Arenas <armando.arenas@snhu.edu>
2025-07-03 00:14:15 -0400

    Scraping not working

--- backend/src/main/java/com/pricecompare/service/SamsClubScraperService.java
+++ backend/src/main/java/com/pricecompare/service/SamsClubScraperService.java
@@ -18,11 +18,12 @@ public class SamsClubScraperService {
         List<String> results = new ArrayList<>();
         String searchUrl = BASE_URL + query.replace(" ", "%20"); // Replace spaces with %20
         Document doc = Jsoup.connect(searchUrl)
-                        .userAgent("Mozilla/5.0")
-                        .timeout(10_000)
+                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+                        .timeout(10000)
                         .get();
 
         Elements productCards = doc.select("div.sc-pc-medium-desktop-card-canary.sc-plp-cards-card"); // CSS Classes to find the product cards from Sam's Club
+        System.out.println(doc.html());
         for (Element product : productCards) {
             String title = product.select("div.sc-pc-title-medium.title-medium-desktop-canary").text();
             String price = product.select("div.sc-pc-medium-desktop-moneybox-price span").text();
